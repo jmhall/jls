6c1162271c714509d5555a1128f560659d5e054c
jmhall <j.matt.hall@gmail.com>
2015-07-06 17:45:08 -0400

    Adding test message

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain'} );
-    res.end('Hello World\n');
+    res.end("Jacob's Ladder - Activity Tracking Test App\n");
 }).listen(port);
